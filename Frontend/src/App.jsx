f4e9ca3ee2515859f8c5a8e909824729519333d5
Kaushik-Sahu <kaushiksahu5454@gmail.com>
2025-11-28 18:09:03 +0530

    inital data retrival

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ServiceProviderPage from './pages/ServiceProviderPage';
+import { useRefreshData } from './hooks/useRefreshData';
 import './index.css';
 
 function App() {
+  const refreshData = useRefreshData();
+
+  useEffect(() => {
+    refreshData();
+  }, [refreshData]);
+
   return (
     <BrowserRouter>
       <div className="App">
