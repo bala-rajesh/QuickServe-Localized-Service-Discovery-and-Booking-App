de12a1ebdf1fa2662e704ec69fdb0bb7e9c443f9
Kaushik-Sahu <kaushiksahu5454@gmail.com>
2025-11-28 17:56:23 +0530

    dependencies update

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,11 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler', {}]],
-      },
-    }),
+    react(),
     tailwindcss(),
   ],
   resolve: {
