954ba89effc190b490a62b4f5231339a48c178ee
Kaushik-Sahu <kaushiksahu5454@gmail.com>
2025-11-29 22:16:00 +0530

    Icons deleted

--- Frontend/src/components/icons/CalendarMonthIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CalendarMonthIcon = () => {
-    return (
-        <span className="material-symbols-outlined text-2xl!">calendar_month</span>
-    );
-};
-
-export default CalendarMonthIcon;
--- Frontend/src/components/icons/SettingsIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SettingsIcon = () => {
-    return (
-        <span className="material-symbols-outlined text-2xl!">settings</span>
-    );
-};
-
-export default SettingsIcon;
