c4aa677c9d234bbd5a3bba3c8832c3fcf5cd1038
Kaushik-Sahu <kaushiksahu5454@gmail.com>
2025-11-29 22:33:38 +0530

    Routing added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,33 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ServiceProviderPage from './pages/ServiceProviderPage';
-import { useRefreshData } from './hooks/useRefreshData';
+import DashboardPage from './pages/Subpages/DashboardPage';
+import ProfileDetailPage from './pages/Subpages/ProfileDetailPage';
+import BookingsPage from './pages/Subpages/BookingsPage';
+import MyServicesPage from './pages/Subpages/MyServicesPage';
+import EarningsPage from './pages/Subpages/EarningsPage';
+import { useLoadInitialData } from './hooks/useBookings';
 import './index.css';
 
 function App() {
-  const refreshData = useRefreshData();
+  const loadInitialData = useLoadInitialData();
 
   useEffect(() => {
-    refreshData();
-  }, [refreshData]);
+    loadInitialData();
+  }, [loadInitialData]);
 
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<ServiceProviderPage />} />
+          <Route path="/" element={<Navigate to="/service-provider/dashboard" />} />
+          <Route path="/service-provider" element={<ServiceProviderPage />}>
+            <Route path="dashboard" element={<DashboardPage />} />
+            <Route path="profile" element={<ProfileDetailPage />} />
+            <Route path="bookings" element={<BookingsPage />} />
+            <Route path="services" element={<MyServicesPage />} />
+            <Route path="earnings" element={<EarningsPage />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
